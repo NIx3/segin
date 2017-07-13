dc39a61b10e0480da81892d1f539f2672469417e
NIx3 <nicolas_ignacio94@hotmail.com>
2017-07-12 21:24:26 -0400

    Add files via upload

---
+++ class_lib/main_fotter.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+        Auditoria y Seguridad en Informatica.
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2017 SegIn - Auditoria y Seguridad en Informatica | <a href=''></a></strong> | All rights reserved.
+      </footer>
